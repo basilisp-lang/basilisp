74846b3b5d7c378d8992570f284cf1029d391c8a
Chris Rink <chrisrink10@users.noreply.github.com>
2018-05-19 21:14:02 -0400

    Add Maybe optional to make it easier to handle None (#3)

--- basilisp/util.py
+++ basilisp/util.py
@@ -1,8 +1,10 @@
 import functools
 import inspect
 import os.path
+from typing import TypeVar, Generic, Optional, Callable
 
 from functional import seq
+from functional.pipeline import Sequence
 
 from basilisp.lang.util import lrepr
 
@@ -35,3 +37,58 @@ def trace(f):
             raise e
 
     return wrapper
+
+
+T = TypeVar('T')
+U = TypeVar('U')
+
+
+class Maybe(Generic[T]):
+    __slots__ = ('_inner', )
+
+    def __init__(self, inner: Optional[T]) -> None:
+        self._inner = inner
+
+    def __eq__(self, other):
+        if isinstance(other, Maybe):
+            return self._inner == other.value
+        return self._inner == other
+
+    def __repr__(self):
+        return repr(self._inner)
+
+    def __str__(self):
+        return str(self._inner)
+
+    def or_else(self, else_fn: Callable[[], T]) -> T:
+        if self._inner is None:
+            return else_fn()
+        return self._inner
+
+    def or_else_get(self, else_v: T) -> T:
+        if self._inner is None:
+            return else_v
+        return self._inner
+
+    def or_else_raise(self, raise_fn: Callable[[], Exception]) -> T:
+        if self._inner is None:
+            raise raise_fn()
+        return self._inner
+
+    def map(self, f: Callable[[T], U]) -> "Maybe[U]":
+        if self._inner is None:
+            return Maybe(None)
+        return Maybe(f(self._inner))
+
+    @property
+    def value(self) -> Optional[T]:
+        return self._inner
+
+    def stream(self) -> Sequence:
+        if self._inner is None:
+            return seq([])
+        return seq([self._inner])
+
+    @property
+    def is_present(self) -> bool:
+        return self._inner is not None
