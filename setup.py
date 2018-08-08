6bbc18f374aa94f957a9fe08d1f2f4f6e59b4bd4
Chris Rink <chrisrink10@users.noreply.github.com>
2018-08-08 12:05:28 -0400

    Fix setup.py (#33)
    * Update setup.py
    
    * Add version metadata to package
    
    * Add Basilisp code files into package

--- setup.py
+++ setup.py
@@ -1,19 +1,125 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import setup, Command, find_packages
+
+# Package metadata
+NAME = 'basilisp'
+DESCRIPTION = 'A Clojure-like lisp written for Python'
+URL = 'http://github.com/chrisrink10/basilisp'
+EMAIL = 'chrisrink10@gmail.com'
+AUTHOR = 'Christopher Rink'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = None
+
+REQUIRED = [
+    'astor',
+    'atomos',
+    'pyfunctional',
+    'pyrsistent',
+    'pytest',
+    'wrapt'
+]
+
+EXTRAS = {}
+
+# Copied from the excellent https://github.com/kennethreitz/setup.py
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
 
 setup(
-    name='basilisp',
-    version='0.1',
-    packages=['basilisp'],
-    url='http://github.com/chrisrink10/basilisp',
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
     license='MIT License',
-    author='Christopher Rink',
-    author_email='chrisrink10@gmail.com',
-    description='A Clojure-like lisp written for Python',
-    install_requires=[
-        'pyrsistent==0.14.0',
-        'pytest==3.2.3'
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
     ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
