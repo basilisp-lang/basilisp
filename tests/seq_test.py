0f35ec39d8134a9a09418ceceaab147da660ca05
Christopher Rink <chrisrink10@gmail.com>
2018-08-21 15:30:48 -0400

    Lazy sequences and tests

--- tests/seq_test.py
+++ tests/seq_test.py
@@ -8,6 +8,61 @@ def test_to_sequence():
     assert llist.l(1, 2, 3) == lseq.sequence([1, 2, 3])
 
 
+def test_lazy_sequence():
+    s = lseq.LazySeq(lambda: None)
+    assert not s.is_empty, "LazySeq has not been realized yet"
+    assert None is s.first
+    assert lseq.empty() == s.rest
+    assert s.realized
+    assert s.is_empty, "LazySeq has been realized and is empty"
+
+    s = lseq.LazySeq(lambda: lseq.empty())
+    assert not s.is_empty, "LazySeq has not been realized yet"
+    assert None is s.first
+    assert lseq.empty() == s.rest
+    assert s.realized
+    assert s.is_empty, "LazySeq has been realized and is empty"
+
+    s = lseq.LazySeq(lambda: lseq.sequence([1]))
+    assert not s.is_empty, "LazySeq has not been realized yet"
+    assert 1 == s.first
+    assert lseq.empty() == s.rest
+    assert s.realized
+    assert not s.is_empty, "LazySeq has been realized and is not empty"
+
+    def lazy_seq():
+        def inner_seq():
+            def inner_inner_seq():
+                return lseq.sequence([3])
+
+            return lseq.LazySeq(inner_inner_seq).cons(2)
+
+        return lseq.LazySeq(inner_seq).cons(1)
+
+    s = lseq.LazySeq(lazy_seq)
+    assert not s.is_empty, "LazySeq has not been realized yet"
+    assert 1 == s.first
+    assert isinstance(s.rest, lseq.LazySeq)
+    assert s.realized
+    assert not s.is_empty, "LazySeq has been realized and is not empty"
+
+    r = s.rest
+    assert not r.is_empty, "LazySeq has not been realized yet"
+    assert 2 == r.first
+    assert isinstance(r.rest, lseq.LazySeq)
+    assert r.realized
+    assert not r.is_empty, "LazySeq has been realized and is not empty"
+
+    t = r.rest
+    assert not t.is_empty, "LazySeq has not been realized yet"
+    assert 3 == t.first
+    assert lseq.empty() == t.rest
+    assert t.realized
+    assert not t.is_empty, "LazySeq has been realized and is not empty"
+
+    assert [1, 2, 3] == [e for e in s]
+
+
 def test_empty_sequence():
     empty = lseq.sequence([])
     assert None is empty.first
