64f44599ec866c49e1b0cd3a456245421c080ba8
Christopher Rink <chrisrink10@gmail.com>
2018-09-13 15:27:06 -0400

    Bump version to 0.1.dev1

--- basilisp/__version__.py
+++ basilisp/__version__.py
@@ -1,3 +1,3 @@
-VERSION = (0, 1, "dev0")
+VERSION = (0, 1, "dev1")
 
 __version__ = '.'.join(map(str, VERSION))
--- setup.py
+++ setup.py
@@ -118,6 +118,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Compilers'
