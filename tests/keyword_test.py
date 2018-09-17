3693cc9d8656989fd2dea261c60a1c47e1cec3b6
Chris Rink <chrisrink10@users.noreply.github.com>
2018-09-17 09:35:50 -0400

    Keywords used in function position should work properly now (#175)

--- tests/keyword_test.py
+++ tests/keyword_test.py
@@ -1,3 +1,4 @@
+import basilisp.lang.map as lmap
 from basilisp.lang.keyword import keyword
 
 
@@ -31,3 +32,12 @@ def test_keyword_str_and_repr():
     kw = keyword("kw")
     assert str(kw) == "kw"
     assert repr(kw) == ":kw"
+
+
+def test_keyword_as_function():
+    kw = keyword("kw")
+    assert None is kw(None)
+
+    assert 1 == kw(lmap.map({kw: 1}))
+    assert "hi" == kw(lmap.map({kw: "hi"}))
+    assert None is kw(lmap.map({"hi": kw}))
