44cabb0ac5ffba3c1b36c3d8cc05256e393d7c54
Christopher Rink <chrisrink10@gmail.com>
2018-09-20 23:38:02 -0400

    Slightly update the tests; still broken

--- tests/basilisp/testrunner_test.py
+++ tests/basilisp/testrunner_test.py
@@ -1,19 +1,30 @@
 import _pytest.pytester as pytester
+import pytest
+
+import basilisp.lang.symbol as sym
+import basilisp.main as basilisp
+from basilisp.lang.runtime import Namespace
 
 
-def test_testrunner(testdir: pytester.Testdir):
+@pytest.fixture
+def core_ns() -> Namespace:
+    basilisp.init()
+    return Namespace.get(sym.symbol('basilisp.core'))
+
+
+def test_testrunner(core_ns: Namespace, testdir: pytester.Testdir):
     code = """
-    (ns fixture.test
+    (ns fixture
       (:require
        [basilisp.test :refer [deftest is]]))
 
-    (deftest fixture
+    (deftest fixture-test
       (is (= 1 1))
       (is (= :hi :hi))
       (is true)
       (is (= "true" false)))
     """
-    testdir.makefile('lpy', code)
+    testdir.makefile('.lpy', fixture=code)
 
-    result: pytester.RunResult = testdir.runpytest()
-    result.assert_outcomes(passed=0, failed=1)
+    result: pytester.RunResult = testdir.runpytest_inprocess()
+    result.assert_outcomes(failed=1)
