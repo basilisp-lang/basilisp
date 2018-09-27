d52b03adb43aed8fa381ebdf8b6be866dc12f4f9
Chris Rink <chrisrink10@users.noreply.github.com>
2018-09-26 21:05:50 -0400

    Demunge namespaces specified via Python import machinery (#217)

--- tests/basilisp/importer_test.py
+++ tests/basilisp/importer_test.py
@@ -1,7 +1,14 @@
+import importlib
+import os.path
 import sys
+import tempfile
 from unittest.mock import patch
 
+import _pytest.pytester as pytester
+
 import basilisp.importer as importer
+import basilisp.lang.runtime as runtime
+import basilisp.lang.symbol as sym
 
 
 def importer_counter():
@@ -22,3 +29,21 @@ def test_hook_imports():
         assert 1 == importer_counter()
         importer.hook_imports()
         assert 1 == importer_counter()
+
+
+def test_demunged_import(testdir: pytester.Testdir):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tmp_module = os.path.join(tmpdir, 'long__AMP__namespace_name__PLUS__with___LT__punctuation__GT__.lpy')
+        with open(tmp_module, mode='w') as module:
+            code = """
+            (ns long&namespace-name+with-<punctuation>)
+            """
+            module.write(code)
+
+        with patch('sys.path', new=[tmpdir]), \
+             patch('sys.meta_path', new=[importer.BasilispImporter()]):
+            importlib.import_module('long__AMP__namespace_name__PLUS__with___LT__punctuation__GT__')
+
+        assert runtime.Namespace.get(sym.symbol('long&namespace-name+with-<punctuation>')) is not None
+        assert runtime.Namespace.get(
+            sym.symbol('long__AMP__namespace_name__PLUS__with___LT__punctuation__GT__')) is None
