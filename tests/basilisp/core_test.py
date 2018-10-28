76e9755dcf9698a6fe2b4c81db957a027c4558e5
Christopher Rink <chrisrink10@gmail.com>
2018-10-27 22:30:10 -0400

    Fix range and partition

--- tests/basilisp/core_test.py
+++ tests/basilisp/core_test.py
@@ -508,8 +508,8 @@ def test_vals():
 
 def test_range():
     assert llist.l(1) == core.range_(1, 1)
-    assert llist.l(1, 2, 3, 4, 5) == core.range_(1, 5)
-    assert llist.l(1, 3, 5, 7, 9) == core.range_(1, 10, 2)
+    assert llist.l(1, 2, 3, 4, 5) == core.range_(1, 6)
+    assert llist.l(1, 3, 5, 7, 9) == core.range_(1, 11, 2)
     # assert llist.l(1, -1, -3, -5, -7, -9) == core.range_(1, -10, -2)
 
 
@@ -632,7 +632,7 @@ def test_map():
     assert llist.l(2, 3, 4) == core.map_(core.inc, vec.v(1, 2, 3))
 
     assert llist.l(5, 7, 9) == core.map_(core.__PLUS__, vec.v(1, 2, 3), vec.v(4, 5, 6))
-    assert llist.l(5, 7, 9) == core.map_(core.__PLUS__, vec.v(1, 2, 3), core.range_(4))
+    assert llist.l(5, 7, 9) == core.map_(core.__PLUS__, vec.v(1, 2, 3), core.range_(4, 7))
 
 
 def test_map_indexed():
@@ -744,19 +744,21 @@ def test_repeatedly():
 
 
 def test_partition():
-    assert llist.l(llist.l(1, 2), llist.l(3, 4), llist.l(5, 6)) == core.partition(2, core.range_(1, 6))
-    assert llist.l(llist.l(1, 2), llist.l(3, 4), llist.l(5, 6)) == core.partition(2, core.range_(1, 6))
+    assert llist.l(llist.l(1, 2), llist.l(3, 4), llist.l(5, 6)) == core.partition(2, core.range_(1, 7))
+    assert llist.l(llist.l(1, 2, 3), llist.l(4, 5, 6)) == core.partition(3, core.range_(1, 7))
 
     assert llist.l(llist.l(1, 2, 3, 4, 5), llist.l(11, 12, 13, 14, 15),
-                   llist.l(21, 22, 23)) == core.partition(5, 10, core.range_(1, 23))
+                   llist.l(21, 22, 23)) == core.partition(5, 10, core.range_(1, 24))
     assert llist.l(llist.l(1, 2, 3, 4, 5), llist.l(11, 12, 13, 14, 15),
-                   llist.l(21, 22, 23, 24, 25)) == core.partition(5, 10, core.range_(1, 25))
+                   llist.l(21, 22, 23, 24, 25)) == core.partition(5, 10, core.range_(1, 26))
 
     assert llist.l(llist.l(1, 2, 3, 4, 5), llist.l(11, 12, 13, 14, 15),
-                   llist.l(21, 22, 23, kw.keyword("a"), kw.keyword("a"))) == core.partition(5, 10, core.range_(1, 23),
-                                                                                            kw.keyword("a"))
+                   llist.l(21, 22, 23, kw.keyword("a"), kw.keyword("a"))) == core.partition(
+        5, 10, core.repeat(kw.keyword("a")), core.range_(1, 24))
     assert llist.l(llist.l(1, 2, 3, 4, 5), llist.l(11, 12, 13, 14, 15),
-                   llist.l(21, 22, 23, 24, 25)) == core.partition(5, 10, core.range_(1, 25), kw.keyword("a"))
+                   llist.l(21, 22, 23, 24, 25)) == core.partition(
+        5, 10, core.repeat(kw.keyword("a")), core.range_(1, 26))
+
 
 def test_pr_str():
     assert '' == core.pr_str()
