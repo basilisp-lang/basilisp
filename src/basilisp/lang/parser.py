831141e8abb2d58d02e4a28ae820a0c673b2358d
Christopher Rink <chrisrink10@gmail.com>
2019-01-26 12:58:31 -0500

    Constant values

--- src/basilisp/lang/parser.py
+++ src/basilisp/lang/parser.py
@@ -24,6 +24,9 @@ LITERAL_Q = kw.keyword("literal?")
 TYPE = kw.keyword("type")
 VAL = kw.keyword("val")
 META = kw.keyword("meta")
+ITEMS = kw.keyword("items")
+KEYS = kw.keyword("keys")
+VALS = kw.keyword("vals")
 
 # Node types
 BINDING = kw.keyword("binding")
@@ -56,20 +59,103 @@ VAR = kw.keyword("var")
 VECTOR = kw.keyword("vector")
 WITH_META = kw.keyword("with-meta")
 
+# Constant node types (not already covered by the above)
+NIL = kw.keyword("nil")
+BOOL = kw.keyword("bool")
+KEYWORD = kw.keyword("keyword")
+SYMBOL = kw.keyword("symbol")
+STRING = kw.keyword("string")
+NUMBER = kw.keyword("number")
+RECORD = kw.keyword("record")
+SEQ = kw.keyword("seq")
+CHAR = kw.keyword("char")
+REGEX = kw.keyword("regex")
+CLASS = kw.keyword("class")
+INST = kw.keyword("inst")
+UUID = kw.keyword("uuid")
+UNKNOWN = kw.keyword("unknown")
+
+
+class ParserException(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+def _map_node(form: lmap.Map) -> lmap.Map:
+    keys, vals = [], []
+    for k, v in form.items():
+        keys.append(parse_ast(k))
+        vals.append(parse_ast(v))
+
+    return lmap.map(
+        {
+            OP: MAP,
+            FORM: form,
+            KEYS: vec.vector(keys),
+            VALS: vec.vector(vals),
+            CHILDREN: vec.v(KEYS, VALS),
+        }
+    )
+
+
+def _set_node(form: lset.Set) -> lmap.Map:
+    return lmap.map(
+        {
+            OP: SET,
+            FORM: form,
+            ITEMS: vec.vector(map(parse_ast, form)),
+            CHILDREN: vec.v(ITEMS),
+        }
+    )
+
+
+def _vector_node(form: vec.Vector) -> lmap.Map:
+    return lmap.map(
+        {
+            OP: VECTOR,
+            FORM: form,
+            ITEMS: vec.vector(map(parse_ast, form)),
+            CHILDREN: vec.v(ITEMS),
+        }
+    )
+
 
 def _const_node(form: LispForm) -> lmap.Map:
-    descriptor = lmap.map({
-        OP: CONST,
-        FORM: form,
-        LITERAL_Q: True,
-        TYPE: "",
-        VAL: form,
-        CHILDREN: vec.Vector.empty()
-    })
+    node_type = {
+        bool: BOOL,
+        complex: NUMBER,
+        datetime: INST,
+        Decimal: NUMBER,
+        float: NUMBER,
+        Fraction: NUMBER,
+        int: NUMBER,
+        kw.Keyword: KEYWORD,
+        Pattern: REGEX,
+        sym.Symbol: SYMBOL,
+        str: STRING,
+        type(None): NIL,
+        uuid.UUID: UUID,
+    }.get(type(form), UNKNOWN)
 
-    if form.meta is not None:
+    descriptor = lmap.map(
+        {
+            OP: CONST,
+            FORM: form,
+            LITERAL_Q: True,
+            TYPE: node_type,
+            VAL: form,
+            CHILDREN: vec.Vector.empty(),
+        }
+    )
+
+    if hasattr(form, "meta") and form.meta is not None:
         meta_ast = parse_ast(form.meta)
-        return descriptor.assoc(META, meta_ast, CHILDREN, vec.v(meta_ast))
+
+        meta_op = meta_ast.entry(OP)
+        if meta_op == MAP or (meta_op == CONST and meta_ast.entry(TYPE) == MAP):
+            return descriptor.assoc(META, meta_ast, CHILDREN, vec.v(META))
+
+        raise ParserException(f"Meta applied to constant must be a map")
 
     return descriptor
 
@@ -86,21 +172,14 @@ def parse_ast(form: LispForm) -> lmap.Map:  # pylint: disable=too-many-branches
     two arguments (a `CompilerContext` object, and the form to compile).
     Each function returns a generator, which may contain 0 or more AST
     nodes."""
-    if isinstance(form, llist.List):
-        yield from _list_ast(ctx, form)
-        return
-    elif isinstance(form, lseq.Seq):
-        yield from _list_ast(ctx, llist.list(form))
-        return
+    if isinstance(form, (llist.List, lseq.Seq)):
+        return lmap.Map.empty()
     elif isinstance(form, vec.Vector):
-        yield from _vec_ast(ctx, form)
-        return
+        return _vector_node(form)
     elif isinstance(form, lmap.Map):
-        yield from _map_ast(ctx, form)
-        return
+        return _map_node(form)
     elif isinstance(form, lset.Set):
-        yield from _set_ast(ctx, form)
-        return
+        return _set_node(form)
     elif isinstance(
         form,
         (
@@ -119,7 +198,6 @@ def parse_ast(form: LispForm) -> lmap.Map:  # pylint: disable=too-many-branches
             uuid.UUID,
         ),
     ):
-        yield from _const_node(form)
-        return
+        return _const_node(form)
     else:
         raise TypeError(f"Unexpected form type {type(form)}: {form}")
