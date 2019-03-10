6dd585d9585651cd64cef8bf4e5dace1a3b5b226
Christopher Rink <chrisrink10@gmail.com>
2019-03-10 18:30:39 -0400

    Remove dead code in the optimizer

--- src/basilisp/lang/compiler/optimizer.py
+++ src/basilisp/lang/compiler/optimizer.py
@@ -1,8 +1,34 @@
 import ast
-from typing import Optional
+from typing import Optional, Iterable, List
+
+
+def _filter_dead_code(nodes: Iterable[ast.AST]) -> List[ast.AST]:
+    """Return a list of body nodes, trimming out unreachable code (any
+    statements appearing after `break`, `continue`, and `return` nodes)."""
+    new_nodes: List[ast.AST] = []
+    for node in nodes:
+        if isinstance(node, (ast.Break, ast.Continue, ast.Return)):
+            new_nodes.append(node)
+            break
+        new_nodes.append(node)
+    return new_nodes
 
 
 class PythonASTOptimizer(ast.NodeTransformer):
+    def visit_ExceptHandler(self, node: ast.ExceptHandler) -> Optional[ast.AST]:
+        new_node = self.generic_visit(node)
+        if not isinstance(new_node, ast.ExceptHandler):
+            return new_node
+        else:
+            return ast.copy_location(
+                ast.ExceptHandler(
+                    type=new_node.type,
+                    name=new_node.name,
+                    body=_filter_dead_code(new_node.body),
+                ),
+                new_node,
+            )
+
     def visit_Expr(self, node: ast.Expr) -> Optional[ast.Expr]:
         """Eliminate no-op constant expressions which are in the tree
         as standalone statements."""
@@ -18,3 +44,62 @@ class PythonASTOptimizer(ast.NodeTransformer):
         ):
             return None
         return node
+
+    def visit_FunctionDef(self, node: ast.FunctionDef) -> Optional[ast.AST]:
+        new_node = self.generic_visit(node)
+        if not isinstance(new_node, ast.FunctionDef):
+            return new_node
+        else:
+            return ast.copy_location(
+                ast.FunctionDef(
+                    name=new_node.name,
+                    args=new_node.args,
+                    body=_filter_dead_code(new_node.body),
+                    decorator_list=new_node.decorator_list,
+                    returns=new_node.returns,
+                ),
+                new_node,
+            )
+
+    def visit_If(self, node: ast.If) -> Optional[ast.AST]:
+        new_node = self.generic_visit(node)
+        if not isinstance(new_node, ast.If):
+            return new_node
+        else:
+            return ast.copy_location(
+                ast.If(
+                    test=new_node.test,
+                    body=_filter_dead_code(new_node.body),
+                    orelse=_filter_dead_code(new_node.orelse),
+                ),
+                new_node,
+            )
+
+    def visit_While(self, node: ast.While) -> Optional[ast.AST]:
+        new_node = self.generic_visit(node)
+        if not isinstance(new_node, ast.While):
+            return new_node
+        else:
+            return ast.copy_location(
+                ast.While(
+                    test=new_node.test,
+                    body=_filter_dead_code(new_node.body),
+                    orelse=_filter_dead_code(new_node.orelse),
+                ),
+                new_node,
+            )
+
+    def visit_Try(self, node: ast.Try) -> Optional[ast.AST]:
+        new_node = self.generic_visit(node)
+        if not isinstance(new_node, ast.Try):
+            return new_node
+        else:
+            return ast.copy_location(
+                ast.Try(
+                    body=_filter_dead_code(new_node.body),
+                    handlers=new_node.handlers,
+                    orelse=_filter_dead_code(new_node.orelse),
+                    finalbody=_filter_dead_code(new_node.finalbody),
+                ),
+                new_node,
+            )
