e85edc8afafa423f6921251236ac977e22647f93
Chris Rink <chrisrink10@users.noreply.github.com>
2019-03-10 16:41:24 -0400

    Use attrs rather than hand-written Exception (#334)

--- src/basilisp/lang/exception.py
+++ src/basilisp/lang/exception.py
@@ -1,27 +1,18 @@
+import attr
+
 import basilisp.lang.map as lmap
 from basilisp.lang.obj import lrepr
 
 
+@attr.s(auto_attribs=True, cmp=False, repr=False, slots=True, str=False)
 class ExceptionInfo(Exception):
-    __slots__ = ("_msg", "_data")
-
-    def __init__(self, message: str, data: lmap.Map) -> None:
-        super().__init__()
-        self._msg = message
-        self._data = data
+    message: str
+    data: lmap.Map
 
     def __repr__(self):
         return (
-            f"basilisp.lang.exception.ExceptionInfo({self._msg}, {lrepr(self._data)})"
+            f"basilisp.lang.exception.ExceptionInfo({self.message}, {lrepr(self.data)})"
         )
 
     def __str__(self):
-        return f"{self._msg} {lrepr(self._data)}"
-
-    @property
-    def data(self) -> lmap.Map:
-        return self._data
-
-    @property
-    def message(self) -> str:
-        return self._msg
+        return f"{self.message} {lrepr(self.data)}"
