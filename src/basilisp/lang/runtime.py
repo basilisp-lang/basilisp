9c0365cdea4f7b548c70d2ccda8356dd64847748
Christopher Rink <chrisrink10@gmail.com>
2019-04-23 08:32:07 -0400

    More defrecord

--- src/basilisp/lang/runtime.py
+++ src/basilisp/lang/runtime.py
@@ -828,7 +828,10 @@ def apply_kw(f, args):
     except TypeError as e:
         logger.debug("Ignored %s: %s", type(e).__name__, e)
 
-    kwargs = to_py(last, lambda kw: munge(kw.name, allow_builtins=True))
+    kwargs = {
+        to_py(k, lambda kw: munge(kw.name, allow_builtins=True)): v
+        for k, v in last.items()
+    }
     return f(*final, **kwargs)
 
 
