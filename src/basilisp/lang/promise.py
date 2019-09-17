edd7e170c4ad8d08213f9c9769a5361b732f184e
Chris Rink <chrisrink10@users.noreply.github.com>
2019-09-16 21:40:40 -0400

    Promises (#440)
    * Promises
    
    * Force too I guess
    
    * Use the correct slots

---
+++ src/basilisp/lang/promise.py
@@ -0,0 +1,36 @@
+import threading
+from typing import Optional, TypeVar
+
+from basilisp.lang.interfaces import IBlockingDeref
+
+T = TypeVar("T")
+
+
+# pylint: disable=assigning-non-slot
+class Promise(IBlockingDeref[T]):
+    __slots__ = ("_condition", "_is_delivered", "_value")
+
+    def __init__(self) -> None:
+        self._condition = threading.Condition()
+        self._is_delivered = False
+        self._value: Optional[T] = None
+
+    def deliver(self, value: T):
+        with self._condition:
+            if not self._is_delivered:
+                self._is_delivered = True
+                self._value = value
+
+    def deref(
+        self, timeout: Optional[float] = None, timeout_val: Optional[T] = None
+    ) -> Optional[T]:
+        with self._condition:
+            if self._condition.wait_for(lambda: self._is_delivered, timeout=timeout):
+                return self._value
+            else:
+                return timeout_val
+
+    @property
+    def is_realized(self) -> bool:
+        with self._condition:
+            return self._is_delivered
---
+++ tests/basilisp/promise_test.py
@@ -0,0 +1,29 @@
+import threading
+
+import basilisp.lang.vector as vec
+from basilisp.lang.promise import Promise
+
+
+def test_promise():
+    v = vec.v(1, 2, 3)
+
+    p = Promise()
+    assert False is p.is_realized
+
+    def set_promise():
+        p.deliver(v)
+
+    assert "not set yet" == p.deref(timeout=0.2, timeout_val="not set yet")
+    assert False is p.is_realized
+
+    t = threading.Thread(target=set_promise)
+    t.start()
+    t.join()
+
+    assert v == p.deref()
+    assert True is p.is_realized
+
+    p.deliver("another value")
+
+    assert v == p.deref()
+    assert True is p.is_realized
