69257634d99cc0028d62a4daad990eea5060b28a
Christopher Rink <chrisrink10@gmail.com>
2023-12-27 18:14:12 -0500

    Define constants

--- src/basilisp/lang/runtime.py
+++ src/basilisp/lang/runtime.py
@@ -72,6 +72,10 @@ NS_VAR_SYM = sym.symbol(NS_VAR_NAME, ns=CORE_NS)
 NS_VAR_NS = CORE_NS
 REPL_DEFAULT_NS = "basilisp.user"
 SUPPORTED_PYTHON_VERSIONS = frozenset({(3, 8), (3, 9), (3, 10), (3, 11), (3, 12)})
+BASILISP_VERSION_STRING = importlib.metadata.version("basilisp")
+BASILISP_VERSION = vec.vector(
+    (int(s) if s.isdigit() else s) for s in BASILISP_VERSION_STRING.split(".")
+)
 
 # Public basilisp.core symbol names
 COMPILER_OPTIONS_VAR_NAME = "*compiler-options*"
@@ -2135,10 +2139,7 @@ def bootstrap_core(compiler_opts: CompilerOpts) -> None:
     Var.intern(
         CORE_NS_SYM,
         sym.symbol(BASILISP_VERSION_VAR_NAME),
-        vec.vector(
-            (int(s) if s.isdigit() else s)
-            for s in importlib.metadata.version("basilisp").split(".")
-        ),
+        BASILISP_VERSION,
         dynamic=True,
         meta=lmap.map(
             {
