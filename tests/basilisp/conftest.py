76d3f7705a84b92fe66e862e781281dfd7d24423
Chris Rink <chrisrink10@gmail.com>
2024-09-19 10:03:52 -0400

    Add support for Python 3.13

--- tests/basilisp/conftest.py
+++ tests/basilisp/conftest.py
@@ -12,7 +12,7 @@ from basilisp.lang import symbol as sym
 from tests.basilisp.helpers import CompileFn, get_or_create_ns
 
 
-@pytest.fixture(params=[3, 4] if sys.version_info < (3, 8) else [3, 4, 5])
+@pytest.fixture(params=[3, 4, 5])
 def pickle_protocol(request) -> int:
     return request.param
 
