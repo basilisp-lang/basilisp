basilisp-lang/basilisp

commit 54b340a991b55607405826e55c3957dcf413412d

Switch Python import machinery to focus primarily on Python modules (#1176)
Fixes #1165 and #1155

This PR changes the Python `importlib` machinery for Basilisp to simply
focus on importing modules, without getting itself too mixed up in the
business of figuring out what a namespace is actually _called_ from the
module name. When `importlib.import_module` is called, Basilisp just
figures out the location of the module on disk and starts loading it up
as if it were a Basilisp module. It sets the newly introduced
`basilisp.core/*import-module*` dynamic Var to the value of the
importing module and lets the Basilisp runtime fetch that value down and
attach it to a new namespace if and when it is appropriate to do so.

There may be some unintended side effects here for strange cases, so
noting those here for now:
* If multiple namespaces are created within a single invocation of
`import.import_lib` (excluding recursive calls for other imports or
requires), a module could be attached to multiple namespaces. I'm not
sure if that's actually an issue, but it's certainly bizarre and could
lead to some unintended consequences. That being said, I don't really
think it is a priority for the project to support this style of bizarre
metaprogramming, so that may be fine.
* Namespace names and module names aren't necessarily "linked" anymore,
although they never were linked directly before due to the required
munging of module names for Python code.
 * Others?

src/basilisp/contrib/pytest/testrunner.py
@@ -57,9 +57,7 @@ def pytest_unconfigure(config):
         runtime.pop_thread_bindings()
 
 
-def pytest_collect_file(  # pylint: disable=unused-argument
-    file_path: Path, path, parent
-):
+def pytest_collect_file(file_path: Path, parent):
     """Primary PyTest hook to identify Basilisp test files."""
     if file_path.suffix == ".lpy":
         if file_path.name.startswith("test_") or file_path.stem.endswith("_test"):
@@ -177,27 +175,21 @@ def _is_package(path: Path) -> bool:
     return False
 
 
-def _get_fully_qualified_module_name(file: Path) -> str:
+def _get_fully_qualified_module_names(file: Path) -> list[str]:
     """Return the fully qualified module name (from the import root) for a module given
     its location.
 
     This works by traversing up the filesystem looking for the top-most package. From
     there, we derive a Python module name referring to the given module path."""
-    top = None
-    for p in file.parents:
-        if _is_package(p):
-            top = p
-        else:
-            break
-
-    if top is None or top == file.parent:
-        return file.stem
-
-    root = top.parent
-    elems = list(file.with_suffix("").relative_to(root).parts)
-    if elems[-1] == "__init__":
-        elems.pop()
-    return ".".join(elems)
+    paths = []
+    for pth in sys.path:
+        root = Path(pth)
+        if file.is_relative_to(root):
+            elems = list(file.with_suffix("").relative_to(pth).parts)
+            if elems[-1] == "__init__":
+                elems.pop()
+            paths.append(".".join(elems))
+    return paths
 
 
 class BasilispFile(pytest.File):
@@ -251,10 +243,21 @@ class BasilispFile(pytest.File):
         self._fixture_manager.teardown()
 
     def _import_module(self) -> runtime.BasilispModule:
-        modname = _get_fully_qualified_module_name(self.path)
-        module = importlib.import_module(modname)
-        assert isinstance(module, runtime.BasilispModule)
-        return module
+        modnames = _get_fully_qualified_module_names(self.path)
+        assert modnames, "Must have at least one module name"
+
+        exc: Optional[ModuleNotFoundError] = None
+        for modname in modnames:
+            try:
+                module = importlib.import_module(modname)
+            except ModuleNotFoundError as e:
+                exc = e
+            else:
+                assert isinstance(module, runtime.BasilispModule)
+                return module
+
+        assert exc is not None, "Must have an exception or module"
+        raise exc
 
     def collect(self):
         """Collect all tests from the namespace (module) given.

tests/basilisp/testrunner_test.py
@@ -261,3 +261,111 @@ def test_fixtures_with_errors(
     pytester.syspathinsert()
     result: pytest.RunResult = pytester.runpytest()
     result.assert_outcomes(passed=passes, failed=failures, errors=errors)
+
+
+def test_ns_in_syspath(pytester: pytest.Pytester, monkeypatch: pytest.MonkeyPatch):
+    runtime.Namespace.remove(sym.symbol("a.test-path"))
+
+    code = """
+    (ns a.test-path
+      (:require
+       [basilisp.test :refer [deftest is]]))
+    (deftest passing-test
+      (is true))
+    (deftest failing-test
+      (is false))
+    """
+    pytester.makefile(".lpy", **{"./test/a/test_path": code})
+    pytester.syspathinsert()
+    # ensure `a` namespace is in sys.path
+    monkeypatch.syspath_prepend(pytester.path / "test")
+    result: pytest.RunResult = pytester.runpytest("test")
+    result.assert_outcomes(passed=1, failed=1)
+
+
+def test_ns_in_syspath_w_src(
+    pytester: pytest.Pytester, monkeypatch: pytest.MonkeyPatch
+):
+    runtime.Namespace.remove(sym.symbol("a.src"))
+    runtime.Namespace.remove(sym.symbol("a.test-path"))
+
+    code_src = """
+    (ns a.src)
+    (def abc 5)
+    """
+
+    code = """
+    (ns a.test-path
+      (:require
+       a.src
+       [basilisp.test :refer [deftest is]]))
+    (deftest a-test (is (= a.src/abc 5)))
+    (deftest passing-test
+      (is true))
+    (deftest failing-test
+      (is false))
+    """
+    # a slightly more complicated setup where packages under namespace
+    # `a` are both in src and test.
+    pytester.makefile(".lpy", **{"./test/a/test_path": code, "./src/a/src": code_src})
+    pytester.syspathinsert()
+    # ensure src and test is in sys.path
+    monkeypatch.syspath_prepend(pytester.path / "test")
+    monkeypatch.syspath_prepend(pytester.path / "src")
+    result: pytest.RunResult = pytester.runpytest("test")
+    result.assert_outcomes(passed=2, failed=1)
+
+
+def test_ns_not_in_syspath(pytester: pytest.Pytester):
+    runtime.Namespace.remove(sym.symbol("a.test-path"))
+
+    code = """
+    (ns a.test-path
+      (:require
+       [basilisp.test :refer [deftest is]]))
+    """
+    pytester.makefile(".lpy", **{"./test/a/test_path": code})
+    pytester.syspathinsert()
+    result: pytest.RunResult = pytester.runpytest("test")
+    assert result.ret != 0
+    result.stdout.fnmatch_lines(["*ModuleNotFoundError: No module named 'test.a'"])
+
+
+def test_ns_with_underscore(pytester: pytest.Pytester):
+    runtime.Namespace.remove(sym.symbol("test_underscore"))
+
+    code = """
+    (ns test_underscore
+      (:require
+       [basilisp.test :refer [deftest is]]))
+    (deftest passing-test
+      (is true))
+    (deftest failing-test
+      (is false))
+    """
+    pytester.makefile(".lpy", test_underscore=code)
+    pytester.syspathinsert()
+    result: pytest.RunResult = pytester.runpytest()
+    result.assert_outcomes(passed=1, failed=1)
+
+
+def test_no_ns(pytester: pytest.Pytester):
+    runtime.Namespace.remove(sym.symbol("abc"))
+
+    code = """
+    (in-ns 'abc)
+    (require '[basilisp.test :refer [deftest is]]))
+    (deftest passing-test
+      (is true))
+    (deftest failing-test
+      (is false))
+    """
+    pytester.makefile(".lpy", test_under=code)
+    pytester.syspathinsert()
+    result: pytest.RunResult = pytester.runpytest()
+    assert result.ret != 0
+    result.stdout.fnmatch_lines(
+        [
+            "*basilisp.lang.compiler.exception.CompilerException: unable to resolve symbol 'require'*"
+        ]
+    )

src/basilisp/importer.py
@@ -297,7 +297,7 @@ class BasilispImporter(MetaPathFinder, SourceLoader):  # pylint: disable=abstrac
         fullname: str,
         loader_state: Mapping[str, str],
         path_stats: Mapping[str, int],
-        ns: runtime.Namespace,
+        module: BasilispModule,
     ) -> None:
         """Load and execute a cached Basilisp module."""
         filename = loader_state["filename"]
@@ -319,7 +319,7 @@ class BasilispImporter(MetaPathFinder, SourceLoader):  # pylint: disable=abstrac
                     filename=filename, opts=runtime.get_compiler_opts()
                 ),
                 compiler.PythonASTOptimizer(),
-                ns,
+                module,
             )
 
     def _exec_module(
@@ -327,7 +327,7 @@ class BasilispImporter(MetaPathFinder, SourceLoader):  # pylint: disable=abstrac
         fullname: str,
         loader_state: Mapping[str, str],
         path_stats: Mapping[str, int],
-        ns: runtime.Namespace,
+        module: BasilispModule,
     ) -> None:
         """Load and execute a non-cached Basilisp module."""
         filename = loader_state["filename"]
@@ -356,7 +356,7 @@ class BasilispImporter(MetaPathFinder, SourceLoader):  # pylint: disable=abstrac
                 compiler.CompilerContext(
                     filename=filename, opts=runtime.get_compiler_opts()
                 ),
-                ns,
+                module,
                 collect_bytecode=all_bytecode.append,
             )
 
@@ -394,21 +394,29 @@ class BasilispImporter(MetaPathFinder, SourceLoader):  # pylint: disable=abstrac
         # a blank module. If we do not replace the module here with the module we are
         # generating, then we will not be able to use advanced compilation features such
         # as direct Python variable access to functions and other def'ed values.
-        ns_name = demunge(fullname)
-        ns: runtime.Namespace = runtime.Namespace.get_or_create(sym.symbol(ns_name))
-        ns.module = module
-        module.__basilisp_namespace__ = ns
+        if fullname == runtime.CORE_NS:
+            ns: runtime.Namespace = runtime.Namespace.get_or_create(runtime.CORE_NS_SYM)
+            ns.module = module
+            module.__basilisp_namespace__ = ns
 
-        # Check if a valid, cached version of this Basilisp namespace exists and, if so,
-        # load it and bypass the expensive compilation process below.
-        if os.getenv(_NO_CACHE_ENVVAR, "").lower() == "true":
-            self._exec_module(fullname, spec.loader_state, path_stats, ns)
-        else:
-            try:
-                self._exec_cached_module(fullname, spec.loader_state, path_stats, ns)
-            except (EOFError, ImportError, OSError) as e:
-                logger.debug(f"Failed to load cached Basilisp module: {e}")
-                self._exec_module(fullname, spec.loader_state, path_stats, ns)
+        # Set the currently importing module so it can be attached to the namespace when
+        # it is created.
+        with runtime.bindings(
+            {runtime.Var.find_safe(runtime.IMPORT_MODULE_VAR_SYM): module}
+        ):
+
+            # Check if a valid, cached version of this Basilisp namespace exists and, if so,
+            # load it and bypass the expensive compilation process below.
+            if os.getenv(_NO_CACHE_ENVVAR, "").lower() == "true":
+                self._exec_module(fullname, spec.loader_state, path_stats, module)
+            else:
+                try:
+                    self._exec_cached_module(
+                        fullname, spec.loader_state, path_stats, module
+                    )
+                except (EOFError, ImportError, OSError) as e:
+                    logger.debug(f"Failed to load cached Basilisp module: {e}")
+                    self._exec_module(fullname, spec.loader_state, path_stats, module)
 
 
 def hook_imports() -> None:

src/basilisp/lang/compiler/__init__.py
@@ -37,6 +37,7 @@ from basilisp.lang.compiler.generator import gen_py_ast, py_module_preamble
 from basilisp.lang.compiler.generator import statementize as _statementize
 from basilisp.lang.compiler.optimizer import PythonASTOptimizer
 from basilisp.lang.interfaces import ISeq
+from basilisp.lang.runtime import BasilispModule
 from basilisp.lang.typing import CompilerOpts, ReaderForm
 from basilisp.lang.util import genname
 from basilisp.util import Maybe
@@ -109,7 +110,6 @@ def compiler_opts(  # pylint: disable=too-many-arguments
 
 
 def _emit_ast_string(
-    ns: runtime.Namespace,
     module: ast.AST,
 ) -> None:  # pragma: no cover
     """Emit the generated Python AST string either to standard out or to the
@@ -125,7 +125,7 @@ def _emit_ast_string(
     if runtime.print_generated_python():
         print(to_py_str(module))
     else:
-        runtime.add_generated_python(to_py_str(module), which_ns=ns)
+        runtime.add_generated_python(to_py_str(module))
 
 
 def _flatmap_forms(forms: Iterable[ReaderForm]) -> Iterable[ReaderForm]:
@@ -156,7 +156,7 @@ def compile_and_exec_form(
         return None
 
     if not ns.module.__basilisp_bootstrapped__:
-        _bootstrap_module(ctx.generator_context, ctx.py_ast_optimizer, ns)
+        _bootstrap_module(ctx.generator_context, ctx.py_ast_optimizer, ns.module)
 
     last = _sentinel
     for unrolled_form in _flatmap_forms([form]):
@@ -181,7 +181,7 @@ def compile_and_exec_form(
         ast_module = ctx.py_ast_optimizer.visit(ast_module)
         ast.fix_missing_locations(ast_module)
 
-        _emit_ast_string(ns, ast_module)
+        _emit_ast_string(ast_module)
 
         bytecode = compile(ast_module, ctx.filename, "exec")
         if collect_bytecode:
@@ -199,7 +199,7 @@ def compile_and_exec_form(
 def _incremental_compile_module(
     optimizer: PythonASTOptimizer,
     py_ast: GeneratedPyAST,
-    ns: runtime.Namespace,
+    module: BasilispModule,
     source_filename: str,
     collect_bytecode: Optional[BytecodeCollector] = None,
 ) -> None:
@@ -213,39 +213,39 @@ def _incremental_compile_module(
         map(_statementize, itertools.chain(py_ast.dependencies, [py_ast.node]))
     )
 
-    module = ast.Module(body=list(module_body), type_ignores=[])
-    module = optimizer.visit(module)
-    ast.fix_missing_locations(module)
+    module_ast = ast.Module(body=list(module_body), type_ignores=[])
+    module_ast = optimizer.visit(module_ast)
+    ast.fix_missing_locations(module_ast)
 
-    _emit_ast_string(ns, module)
+    _emit_ast_string(module_ast)
 
-    bytecode = compile(module, source_filename, "exec")
+    bytecode = compile(module_ast, source_filename, "exec")
     if collect_bytecode:
         collect_bytecode(bytecode)
-    exec(bytecode, ns.module.__dict__)  # pylint: disable=exec-used  # nosec 6102
+    exec(bytecode, module.__dict__)  # pylint: disable=exec-used  # nosec 6102
 
 
 def _bootstrap_module(
     gctx: GeneratorContext,
     optimizer: PythonASTOptimizer,
-    ns: runtime.Namespace,
+    module: BasilispModule,
     collect_bytecode: Optional[BytecodeCollector] = None,
 ) -> None:
     """Bootstrap a new module with imports and other boilerplate."""
     _incremental_compile_module(
         optimizer,
-        py_module_preamble(ns),
-        ns,
+        py_module_preamble(),
+        module,
         source_filename=gctx.filename,
         collect_bytecode=collect_bytecode,
     )
-    ns.module.__basilisp_bootstrapped__ = True
+    module.__basilisp_bootstrapped__ = True
 
 
 def compile_module(
     forms: Iterable[ReaderForm],
     ctx: CompilerContext,
-    ns: runtime.Namespace,
+    module: BasilispModule,
     collect_bytecode: Optional[BytecodeCollector] = None,
 ) -> None:
     """Compile an entire Basilisp module into Python bytecode which can be
@@ -255,7 +255,7 @@ def compile_module(
     Basilisp import machinery, to allow callers to import Basilisp modules from
     Python code.
     """
-    _bootstrap_module(ctx.generator_context, ctx.py_ast_optimizer, ns)
+    _bootstrap_module(ctx.generator_context, ctx.py_ast_optimizer, module)
 
     for form in _flatmap_forms(forms):
         nodes = gen_py_ast(
@@ -264,7 +264,7 @@ def compile_module(
         _incremental_compile_module(
             ctx.py_ast_optimizer,
             nodes,
-            ns,
+            module,
             source_filename=ctx.filename,
             collect_bytecode=collect_bytecode,
         )
@@ -274,7 +274,7 @@ def compile_bytecode(
     code: list[types.CodeType],
     gctx: GeneratorContext,
     optimizer: PythonASTOptimizer,
-    ns: runtime.Namespace,
+    module: BasilispModule,
 ) -> None:
     """Compile cached bytecode into the given module.
 
@@ -282,9 +282,9 @@ def compile_bytecode(
     namespaces. When the cached bytecode is reloaded from disk, it needs to be
     compiled within a bootstrapped module. This function bootstraps the module
     and then proceeds to compile a collection of bytecodes into the module."""
-    _bootstrap_module(gctx, optimizer, ns)
+    _bootstrap_module(gctx, optimizer, module)
     for bytecode in code:
-        exec(bytecode, ns.module.__dict__)  # pylint: disable=exec-used  # nosec 6102
+        exec(bytecode, module.__dict__)  # pylint: disable=exec-used  # nosec 6102
 
 
 _LOAD_SYM = sym.symbol("load", ns=runtime.CORE_NS)

src/basilisp/lang/compiler/generator.py
@@ -3916,13 +3916,13 @@ def gen_py_ast(ctx: GeneratorContext, lisp_ast: Node) -> GeneratedPyAST[ast.expr
 #############################
 
 
-def _module_imports(ns: runtime.Namespace) -> Iterable[ast.Import]:
+def _module_imports() -> Iterable[ast.Import]:
     """Generate the Python Import AST node for importing all required
     language support modules."""
     # Yield `import basilisp` so code attempting to call fully qualified
     # `basilisp.lang...` modules don't result in compiler errors
     yield ast.Import(names=[ast.alias(name="basilisp", asname=None)])
-    for s in sorted(ns.imports.keys(), key=lambda s: s.name):
+    for s in sorted(runtime.Namespace.DEFAULT_IMPORTS, key=lambda s: s.name):
         name = s.name
         alias = _MODULE_ALIASES.get(name, None)
         yield ast.Import(names=[ast.alias(name=name, asname=alias)])
@@ -3970,10 +3970,10 @@ def _ns_var(
     )
 
 
-def py_module_preamble(ns: runtime.Namespace) -> GeneratedPyAST:
+def py_module_preamble() -> GeneratedPyAST:
     """Bootstrap a new module with imports and other boilerplate."""
     preamble: list[PyASTNode] = []
-    preamble.extend(_module_imports(ns))
+    preamble.extend(_module_imports())
     preamble.extend(_from_module_imports())
     preamble.append(_ns_var())
     return GeneratedPyAST(node=ast.Constant(None), dependencies=preamble)

src/basilisp/lang/runtime.py
@@ -62,6 +62,8 @@ CORE_NS = "basilisp.core"
 CORE_NS_SYM = sym.symbol(CORE_NS)
 NS_VAR_NAME = "*ns*"
 NS_VAR_SYM = sym.symbol(NS_VAR_NAME, ns=CORE_NS)
+IMPORT_MODULE_VAR_NAME = "*import-module*"
+IMPORT_MODULE_VAR_SYM = sym.symbol(IMPORT_MODULE_VAR_NAME, ns=CORE_NS)
 NS_VAR_NS = CORE_NS
 REPL_DEFAULT_NS = "basilisp.user"
 SUPPORTED_PYTHON_VERSIONS = frozenset({(3, 9), (3, 10), (3, 11), (3, 12), (3, 13)})
@@ -826,9 +828,15 @@ class Namespace(ReferenceBase):
         if ns is not None:
             return ns_cache
         new_ns = Namespace(name, module=module)
-        # The `ns` macro is important for setting up an new namespace,
-        # but it becomes available only after basilisp.core has been
-        # loaded.
+
+        # If this is a new namespace and we're given a module (typically from the
+        # importer), attach the namespace to the module.
+        if module is not None:
+            logger.debug(f"Setting module '{module}' namespace to '{new_ns}'")
+            module.__basilisp_namespace__ = new_ns
+
+        # The `ns` macro is important for setting up a new namespace, but it becomes
+        # available only after basilisp.core has been loaded.
         ns_var = Var.find_in_ns(CORE_NS_SYM, sym.symbol("ns"))
         if ns_var:
             new_ns.add_refer(sym.symbol("ns"), ns_var)
@@ -842,6 +850,12 @@ class Namespace(ReferenceBase):
         """Get the namespace bound to the symbol `name` in the global namespace
         cache, creating it if it does not exist.
         Return the namespace."""
+        if (
+            module is None
+            and (module_var := Var.find(IMPORT_MODULE_VAR_SYM)) is not None
+        ):
+            module = module_var.value
+            assert module is None or isinstance(module, BasilispModule)
         return cls._NAMESPACES.swap(Namespace.__get_or_create, name, module=module)[
             name
         ]
@@ -2398,6 +2412,17 @@ def bootstrap_core(compiler_opts: CompilerOpts) -> None:
     )
     Var.intern(
         CORE_NS_SYM, sym.symbol("in-ns"), in_ns, meta=lmap.map({_REDEF_META_KEY: True})
+    )
+    Var.intern(
+        CORE_NS_SYM,
+        sym.symbol(IMPORT_MODULE_VAR_NAME),
+        None,
+        dynamic=True,
+        meta=lmap.map(
+            {
+                _DOC_META_KEY: "If not ``nil``, corresponds to the module which is currently being imported."
+            }
+        ),
     )
 
     # Dynamic Var examined by the compiler when importing new Namespaces

commit 3194c852e765ff6d07ef79627d346fd4ffe884a0

fix testrunner to handle relative entries in sys.path (#1205)
Hi,

could you please review patch to fix a likely regression issue where the
testrunner couldn't handle relative paths in `sys.path`. It fixes #1204.

This is likely a regression from #1176, where the update to the new
module loading system caused relative paths to never match a fully
qualified module name. The patch now expands relative paths to their
absolute form before comparing them to the module path.

This issue was breaking the vanilla `basilisp test` command, as it adds
an empty string (`""`) entry to `sys.path`, which is interpreted as
`"."`. Since this is a relative path, it wasn’t handled by the logic,
causing the project directory not to be recognized as the root of the
Basilisp test modules.

I’ve added a test that invokes `basilisp test` directly as a subprocess.
Unfortunately, I couldn’t find a way to control the pytester `sys.path`
entries with the precise control required for this test. Any counter
suggestions are most welcome.

Thanks

---------

Co-authored-by: ikappaki <ikappaki@users.noreply.github.com>

src/basilisp/cli.py
@@ -745,7 +745,7 @@ def test(
             "Cannot run tests without dependency PyTest. Please install PyTest and try again.",
         )
     else:
-        pytest.main(args=list(extra))
+        sys.exit(pytest.main(args=list(extra)))
 
 
 @_subcommand(
@@ -768,7 +768,9 @@ def test(
         If all options are unambiguous (e.g. they are only either used by Basilisp
         or by PyTest), then you can omit the `--`:
 
-            `basilisp test -k vector -p other_dir`"""
+            `basilisp test -k vector -p other_dir`
+
+        Returns the PyTest exit code as the exit code."""
     ),
     handler=test,
     allows_extra=True,

src/basilisp/contrib/pytest/testrunner.py
@@ -17,6 +17,7 @@ from basilisp.lang import runtime as runtime
 from basilisp.lang import symbol as sym
 from basilisp.lang import vector as vec
 from basilisp.lang.obj import lrepr
+from basilisp.lang.util import munge
 from basilisp.util import Maybe
 
 _EACH_FIXTURES_META_KW = kw.keyword("each-fixtures", "basilisp.test")
@@ -183,9 +184,10 @@ def _get_fully_qualified_module_names(file: Path) -> list[str]:
     there, we derive a Python module name referring to the given module path."""
     paths = []
     for pth in sys.path:
-        root = Path(pth)
+        root = Path(pth).resolve()
         if file.is_relative_to(root):
-            elems = list(file.with_suffix("").relative_to(pth).parts)
+            elems = list(file.with_suffix("").relative_to(root).parts)
+
             if elems[-1] == "__init__":
                 elems.pop()
             paths.append(".".join(elems))
@@ -269,6 +271,12 @@ class BasilispFile(pytest.File):
         filename = self.path.name
         module = self._import_module()
         ns = module.__basilisp_namespace__
+
+        # Ensure the test module was loaded because it was directly
+        # relative to an entry in `sys.path`.
+        if module.__name__ != munge(str(ns)):
+            raise ModuleNotFoundError(f"Module named '{ns}' is not in sys.path")
+
         once_fixtures, each_fixtures = self._collected_fixtures(ns)
         self._fixture_manager = FixtureManager(once_fixtures)
         for test in self._collected_tests(ns):

tests/basilisp/testrunner_test.py
@@ -1,4 +1,6 @@
 import platform
+import shutil
+import subprocess
 import sys
 
 import pytest
@@ -263,6 +265,30 @@ def test_fixtures_with_errors(
     result.assert_outcomes(passed=passes, failed=failures, errors=errors)
 
 
+def test_basilisp_test_noargs(pytester: pytest.Pytester):
+    runtime.Namespace.remove(sym.symbol("a.test-path"))
+
+    code = """
+    (ns tests.test-path
+      (:require
+       [basilisp.test :refer [deftest is]]))
+    (deftest passing-test
+      (is true))
+    """
+    pytester.makefile(".lpy", **{"./tests/test_path": code})
+
+    # I couldn't find a way to directly manipulate the pytester's
+    # `sys.path` with the precise control needed by this test, so we're
+    # invoking `basilisp test` directly as a subprocess instead ...
+    basilisp = shutil.which("basilisp")
+    cmd = [basilisp, "test"]
+    result = subprocess.run(cmd, capture_output=True, text=True, cwd=pytester.path)
+
+    assert "==== 1 passed" in result.stdout.strip()
+
+    assert result.returncode == 0
+
+
 def test_ns_in_syspath(pytester: pytest.Pytester, monkeypatch: pytest.MonkeyPatch):
     runtime.Namespace.remove(sym.symbol("a.test-path"))
 
@@ -324,11 +350,18 @@ def test_ns_not_in_syspath(pytester: pytest.Pytester):
       (:require
        [basilisp.test :refer [deftest is]]))
     """
-    pytester.makefile(".lpy", **{"./test/a/test_path": code})
+    # In this test, we use a `testabc` directory instead of `test`, as
+    # the latter can cause issues on macOS.  Specifically, macOS has a
+    # `/Library/Frameworks/Python.framework/Versions/3.xx/lib/python3.13/test`
+    # directory is picked up, resulting in a slightly different error
+    # message.
+    pytester.makefile(".lpy", **{"./testabc/a/test_path": code})
     pytester.syspathinsert()
-    result: pytest.RunResult = pytester.runpytest("test")
+    result: pytest.RunResult = pytester.runpytest("testabc")
     assert result.ret != 0
-    result.stdout.fnmatch_lines(["*ModuleNotFoundError: No module named 'test.a'"])
+    result.stdout.fnmatch_lines(
+        ["*ModuleNotFoundError: Module named 'a.test-path' is not in sys.path"]
+    )
 
 
 def test_ns_with_underscore(pytester: pytest.Pytester):