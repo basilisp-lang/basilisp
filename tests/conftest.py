a98d8e083b89ee13d4f0b4ee6130d32e403e5f2b
ikappaki <ikappaki@users.noreply.github.com>
2025-02-16 14:23:44 +0000

    conftest.py format

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,18 @@ import pytest
 
 pytest_plugins = ["pytester"]
 
+
 def pytest_configure(config):
     config.addinivalue_line("markers", "slow: Marks tests as slow")
+
 
 def pytest_addoption(parser):
     parser.addoption("--run-slow", action="store_true", help="Run slow tests")
 
+
 @pytest.fixture(autouse=True)
 def skip_slow(request):
-    if request.node.get_closest_marker("slow") and not request.config.getoption("--run-slow"):
+    if request.node.get_closest_marker("slow") and not request.config.getoption(
+        "--run-slow"
+    ):
         pytest.skip("Skipping slow test. Use --run-slow to enable.")
