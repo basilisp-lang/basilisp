eae96dd0095d54c61a073dd0d1adec6838e41f65
ikappaki <ikappaki@users.noreply.github.com>
2025-10-29 20:34:28 +0000

    Restore the original pytest version range

--- src/basilisp/cli.py
+++ src/basilisp/cli.py
@@ -748,7 +748,8 @@ def test(
         # `basilisp` declares the testrunner as a pytest plugin, so
         # pytest tries to import it for assertion rewriting.  Since
         # it's already imported, pytest emits a warning. As rewriting
-        # isn't needed, we ignore it.
+        # isn't needed, we ignore it. (Requires pytest >=8.4.0 to take
+        # effect)
         extra = [
             "-W",
             "ignore:Module already imported so cannot be rewritten; basilisp:pytest.PytestAssertRewriteWarning",
