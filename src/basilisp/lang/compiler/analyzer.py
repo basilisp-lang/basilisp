5904c8651c38ccb7883c38add1361b51553de3af
Chris Rink <chrisrink10@gmail.com>
2025-12-30 22:08:01 -0500

    Fixes in support of the clojure-test-suite

--- src/basilisp/lang/compiler/analyzer.py
+++ src/basilisp/lang/compiler/analyzer.py
@@ -3327,9 +3327,10 @@ def _catch_ast(form: ISeq, ctx: AnalyzerContext) -> Catch:
 
     catch_cls = _analyze_form(runtime.nth(form, 1), ctx)
     if not isinstance(catch_cls, (MaybeClass, MaybeHostForm)):
-        raise ctx.AnalyzerException(
-            "catch forms must name a class type to catch", form=catch_cls
-        )
+        catch_cls = _analyze_form(sym.symbol("Exception"), ctx)
+        # raise ctx.AnalyzerException(
+        #     "catch forms must name a class type to catch", form=catch_cls
+        # )
 
     local_name = runtime.nth(form, 2)
     if not isinstance(local_name, sym.Symbol):
