bfa19f2a1c9d9124ed43c96bbe766efe66d5519f
Chris Rink <chrisrink10@gmail.com>
2026-01-17 13:11:52 -0500

    Revert support for `clojure.test` style test fixtures

--- tests/basilisp/testrunner_test.py
+++ tests/basilisp/testrunner_test.py
@@ -180,9 +180,9 @@ def test_fixtures(pytester: pytest.Pytester):
     (def each-cleanup (volatile! 0))
 
     ;; return here rather than yielding
-    (defn once-fixture-no-cleanup [tests]
+    (defn once-fixture-no-cleanup []
       (vswap! once-no-cleanup inc)
-      (tests))
+      (yield))
 
     (defn once-fixture-w-cleanup []
       (vswap! once-cleanup inc)
